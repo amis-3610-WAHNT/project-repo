bc4e6ecca755f5db417a4762a3b3e1e1e6c5f623
William Turon <wturon1@gmail.com>
2020-03-12 16:59:16 -0400

    Styling and IconsDetails additions

--- src/MainIcons/IconCard.js
+++ src/MainIcons/IconCard.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-
+    
 export default function IconsCard(props) {
     return (
-        <Card>
-            <Card.Body>
-                <Card.Title>{props.cocktail.name}</Card.Title>
-                <Card.Text>{props.cocktail.directions}</Card.Text>
-            </Card.Body>
-        </Card>
+            <Card>
+                <Card.Body>
+                    <Card.Title>{props.cocktail.name}</Card.Title>
+                    <Card.Text>{props.cocktail.directions}</Card.Text>
+                </Card.Body>
+            </Card>
     );
 }
