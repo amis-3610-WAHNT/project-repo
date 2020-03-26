8c453a450831cbf981780b3e97bf8047416a5028
Will Turon <wturon1@gmail.com>
2020-03-26 00:04:04 -0400

    Arrow center

--- src/MainIcons/IconCard.js
+++ src/MainIcons/IconCard.js
@@ -9,11 +9,15 @@ export default function IconsCard(props) {
                 <Card.Body>
                     <Row type="flex">
                         <Col md={11}>
+                            {/* This is header and main text of the card */}
                             <Card.Title>{props.cocktail.name}</Card.Title>
                             <Card.Text>{props.cocktail.directions}</Card.Text>
                         </Col>
                         <Col md={1}>
+                            <div class="vertical-center">
+                            {/* This is the icon that I'm trying to vertically align */}
                             <MDBIcon icon="chevron-right" />
+                            </div>
                         </Col>
                     </Row>
                 </Card.Body>
